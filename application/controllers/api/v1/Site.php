06bc6196f9f0e140e28591993310b3dac5a996cb
kohei-kp <cowhey1991@gmail.com>
2017-10-15 11:04:53 +0900

    サイト追加

---
+++ application/controllers/api/v1/Site.php
@@ -0,0 +1,43 @@
+<?php
+
+class Site extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * サイト一覧を取得する
+     *
+     */
+    public function search()
+    {
+    }
+
+    /**
+     * サイトを登録する
+     *
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * サイトを編集する
+     *
+     */
+    public function edit()
+    {
+    }
+
+    /**
+     * サイトを削除する
+     *
+     */
+    public function delete()
+    {
+    }
+
+}
