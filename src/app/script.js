e1a58981199ac59be206ef5bed80a41d72a1b0ae
kohei-kp <cowhey1991@gmail.com>
2017-11-23 17:17:08 +0900

    ElementUI追加

--- src/app/script.js
+++ src/app/script.js
@@ -1,14 +1,7 @@
-import GraphList from '../GraphList/index.vue';
-
 export default {
   name: 'App',
-  components: {
-    'graph-list': GraphList,
-  },
   data: () => {
     return {
     }
   },
-  props: {
-  }
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon.vue';
 import AppComponent from './app/index.vue';
 
 Vue.use(ElementUI);
+Vue.Component('icon', Icon);
 
 const vm = new Vue({
   el: '#app',
   components: {
-    app: AppComponent
+    app: AppComponent,
   },
   render: h => h('app'),
 });
