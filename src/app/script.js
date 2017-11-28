4e39779043a92af9482653bda7bc669d214e314f
kohei-kp <cowhey1991@gmail.com>
2017-11-28 14:35:42 +0900

    sidemenuにダミーデータを表示

--- src/app/script.js
+++ src/app/script.js
@@ -20,7 +20,48 @@ export default {
           cardName: 'Card Name4',
           src: '/assets/screenshot/5.jpg'
         }
-      ]
+      ],
+      expansions: [
+        {
+          name: 'イクサラン',
+        },
+        {
+          name: '破滅の刻',
+        },
+        {
+          name: 'アモンケット',
+        },
+        {
+          name: '霊気紛争',
+        },
+        {
+          name: 'カラデシュ',
+        },
+        {
+          name: '異界月',
+        },
+        {
+          name: 'イニストラードを覆う影',
+        },
+        {
+          name: 'ゲートウォッチの誓い',
+        },
+        {
+          name: '戦乱のゼンディカー',
+        },
+        {
+          name: 'タルキール龍紀伝',
+        },
+        {
+          name: '運命再編',
+        },
+        {
+          name: 'タルキール覇王譚',
+        },
+        {
+          name: 'ニクスへの旅',
+        }
+      ],
     }
   },
   methods: {
