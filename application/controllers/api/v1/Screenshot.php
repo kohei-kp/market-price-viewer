f6bd5d19cca92a3e553182ecbcc4e13ba93c4b98
kohei-kp <cowhey1991@gmail.com>
2017-12-06 01:41:36 +0900

    スクリーンショット更新用API作成

---
+++ application/controllers/api/v1/Screenshot.php
@@ -0,0 +1,48 @@
+<?php
+
+class Screenshot extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('db_card');
+        $this->load->model('db_site');
+        $this->load->helper('util');
+        $this->load->library('lib_screenshot');
+    }
+
+    /**
+     * 画像を更新する
+     *
+     */
+    public function update()
+    {
+        $this->response->result = TRUE;
+
+        $commit_date = get_datetime();
+
+        $this->db_site->db->trans_start();
+
+        $site = $this->db_site->search(['site_id' => element('siteId', $_POST)]);
+
+        $this->lib_screenshot->takeScreenshot(
+            element('url', $_POST),
+            $site[0]->width,
+            $site[0]->height,
+            $site[0]->top,
+            $site[0]->left,
+            element('cardId', $_POST)
+        );
+
+        $this->db_card->update(
+            element('cardId', $_POST),
+            [ 'update_date' => $commit_date ]
+        );
+
+        $this->db_site->db->trans_complete();
+
+        $this->response->update_date = $commit_date;
+        $this->output_json();
+    }
+}
--- application/libraries/Lib_screenshot.php
+++ application/libraries/Lib_screenshot.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/../../vendor/autoload.php');
 
 use JonnyW\PhantomJs\Client;
 
-class Screenshot
+class Lib_screenshot
 {
 
     private $client;
