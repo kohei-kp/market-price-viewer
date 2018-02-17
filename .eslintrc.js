463fe5afbd02373d1d5312795a7c4a9a09a67283
kohei-kp <cowhey1991@gmail.com>
2018-02-17 17:49:58 +0900

    chore: ESLint導入。 config standard

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+      'standard'
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
